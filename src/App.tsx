9208013cd43608fbc66f2c0969fcb20148b9b59a
Michaelbrima <157252258+Michaelbrima@users.noreply.github.com>
2025-01-24 04:06:41 -0500

    finished mostly working product

--- src/App.tsx
+++ src/App.tsx
@@ -18,77 +18,39 @@ function App() {
   const [newTab, setNewTab] = React.useState("")
   const [newLink, setNewLink] = React.useState("")
   const [newEntry, setNewEntry] = React.useState([]);
-  // function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
-  //   throw new Error('Function not implemented.');
-  // }
+
 
   function handleSubmit(e: { preventDefault: () => void; } | React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>){
     e.preventDefault() //prevents page from refreshing
-
-    // setNewEntry(currentTabs => {
-    //   return [
-    //     ...currentTabs,
-    //     {id: crypto.randomUUID(), entryTitle: newEntry, completed: false },
-    //   ]
-    // })
-    
-    // setNewEntry(newEntry.map(title => newTab))
-//     setNewEntry(
-//       newEntry.map((tab) => {
-//       return {id: crypto.randomUUID(), title: newTab, text: newLink, completed: false};
-//       return newEntry; 
-//     }
-    
-//   )
-// );
-// setNewEntry((previousValues) => ({...newEntry, [event.target.name]: event.target.value, })
-//   )
 
   var formInputs = {
     title: newTab,
     link: newLink,
   };
 
-      // setNewEntry(currentTabs => {
-    //   return [
-    //     ...currentTabs,
-    //     {id: crypto.randomUUID(), entryTitle: newEntry, completed: false },
-    //   ]
-    // })
-
-    // var arrayLocalstorage = [];
-    // arrayLocalstorage.push(formInputs);
 
     setNewEntry([...newEntry, newTab, newLink]);
-    // var arrayLocalstorage = [];
-    var arrayLocalstorage = JSON.parse(localStorage.getItem('formInputs') || '[]');
-    // arrayLocalstorage.push(newEntry);
-    arrayLocalstorage.push(formInputs)
-  // localStorage.arrayOnWebpage.push(formInputs);
-  //  localStorage.setItem("formValuesSubmitted", JSON.stringify(formInputs)); //where array should be set up
-  // localStorage.setItem('arrayOnWebpage', JSON.stringify(arrayLocalstorage));
-  localStorage.setItem('formInputs', JSON.stringify(arrayLocalstorage));
-  // localStorage.setItem('tab', JSON.stringify(formInputs));
-   // const storedFormData = JSON.parse(localStorage.getItem('formInputs'))
-  // storedFormData.push(formInputs);
-  // localStorage.setItem('formInputs', JSON.stringify(storedFormData));
-  alert('Form values submitted to localstorage.')
-  // console.log(newEntry)
 
+    var arrayLocalstorage = JSON.parse(localStorage.getItem('formInputs') || '[]');
+    arrayLocalstorage.push(formInputs)
+
+  localStorage.setItem('formInputs', JSON.stringify(arrayLocalstorage));
+
+  alert('Form values submitted to localstorage.')
   
 }
 
-// const handleChange = (e: { target: { name: any; value: any; }; }) => {
-//   const { name , value } = e.target;
-//   setNewEntry((prev) => ({ ...prev, [name]: value}))
-// }
+
 
 React.useEffect(() => { //update the ;oncancel;storage whenever the newEntry array changes
   localStorage.setItem('newEntry', JSON.stringify(newEntry));
   },
 [newEntry]);
+var tabList = localStorage.getItem("formInputs");
+var regularLists = JSON.parse(tabList);
 
   return (
+
     <div className="App">
       <header className="App-header">
 
@@ -104,39 +66,22 @@ React.useEffect(() => { //update the ;oncancel;storage whenever the newEntry arr
 
         <h1 className="header">Multi Tab Browser</h1>
         <ul className="tabList">
-          {/* {newEntry.push({id: crypto.randomUUID(), title: newTab, text: newLink, completed: false})} */}
-          <li>
-            <label id="tabTitle">Tab Title:</label>
-            <input type="text" value={newTab} id="tabTitle" placeholder='Please enter tab title here.'></input>
-            <iframe src={newLink}></iframe>
-            <button className="closeButton">CLOSE</button>
-          </li>
-          <li>
-            <label id="tabTitle">Tab Title:</label>
-            <input type="text" id="tabTitle" placeholder='Please enter tab title here.'></input>
-            <iframe src={newLink}></iframe>
-            <button className="closeButton">CLOSE</button>
-          </li>
+               {regularLists.map((regularList: any) => (
+      <li>
+   <label id="tabTitle">Tab Title:</label>
+   <input type="text" value={regularList.title} id="tabTitle" placeholder='Please enter tab title here.'></input>
+   <iframe src={regularList.link}></iframe>
+   <button className="closeButton">CLOSE</button>
+ </li>
+))}
+          {/* {           
+         localStorage.getItem("formInputs")
+ } */}
+ 
+ 
         </ul>
         
-        {/* <form>
-        <label id="tabTitle">Tab Title:</label>
-                    <input type="text" id="tabTitle" placeholder='Please enter tab title here.'></input>
-                    <br></br>
-                    <input type="text" id="tabLink" placeholder='Please enter the link you would like to be featured here.'></input>
-                    <br></br>
-                    <iframe src=""></iframe>
-                    <br></br>
-                    <button className="btn btn-danger">Submit</button>
-                    </form> */}
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+
         <Link href="https://reactjs.org" targetBlank>Learn React</Link> {/*targetBlank is true by default*/}
       </header>
     </div>
